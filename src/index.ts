7b323091d050f263db9df584bb7c67da5402144c
Zampou Ibrahim <zampou@192.168.1.4>
2022-11-25 18:03:05 +0000

    Add truc 2

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 // here you put all the js you want.
+
+window.onload = () => {
+  let menu = document.querySelector('.navbar .navbar__menu-icon');
+  let nav = document.querySelector('.navbar > .navbar-content');
+  
+  menu?.addEventListener('click', () => {
+    nav?.classList.toggle('mobile')
+  })
+}
+
