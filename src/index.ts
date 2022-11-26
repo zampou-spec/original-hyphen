f5bd6c7f440e7434cff693a946ca861799b96bab
Zampou Ibrahim <zampou@192.168.1.4>
2022-11-26 01:26:56 +0000

    push correctif 1.2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 // here you put all the js you want.
 
-window.onload = () => {
-  let menu = document.querySelector('.navbar .navbar__menu-icon');
-  let nav = document.querySelector('.navbar > .navbar-content');
-  
+document.addEventListener('load', () => {
+  const pathname = window.location.pathname;
+  const nav = document.querySelector('.navbar > .navbar-content');
+  const menu = document.querySelector('.navbar .navbar__menu-icon');
+  const lis = document.querySelectorAll('.navbar-content__menu li');
+
+  lis.forEach((li) => {
+    li.classList.remove('active')
+  })
+
+  lis.forEach((li) => {
+    if (li.children[0]?.pathname == pathname) {
+      li.classList.add('active')
+    }
+  })
+
   menu?.addEventListener('click', () => {
     nav?.classList.toggle('mobile')
   })
-}
-
+})
